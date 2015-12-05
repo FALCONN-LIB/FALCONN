dc6f9de5686f9fece655f6bd0166ef4838a51196
Ludwig Schmidt <ludwigschmidt2@gmail.com>
2015-12-04 20:33:21 -0500

    adding the test script

---
+++ test-script.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+FILENAME="test-output.txt"
+
+make clean 2>&1 | tee $FILENAME
+make run_all_tests 2>&1 | tee -a $FILENAME
+printf "\n\n=========================================================================\n\n\n" | tee -a $FILENAME
+make random_benchmark 2>&1 | tee -a $FILENAME
+./random_benchmark 2>&1 | tee -a $FILENAME
