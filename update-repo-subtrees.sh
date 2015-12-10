68b0331df3ed1d5c9cf85f5b76d58efd2e9fee00
Ludwig Schmidt <ludwigschmidt2@gmail.com>
2015-12-10 09:46:27 -0500

    adding script for updating repo subtrees

---
+++ update-repo-subtrees.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+git subtree pull --prefix src/include/falconn/ffht https://github.com/FALCONN-LIB/FFHT.git master --squash
+git subtree pull --prefix external/eigen https://github.com/FALCONN-LIB/eigen-mirror.git master --squash
+git subtree pull --prefix external/googletest https://github.com/google/googletest.git master --squash
