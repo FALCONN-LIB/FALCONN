bf4ecaec299b625b50bb266e200282768c15e99a
Ludwig Schmidt <ludwigschmidt2@gmail.com>
2015-12-26 22:33:06 +0100

    adding unit tests to make sure that the subsequence iterator of the plain array data storage works

--- src/test/data_storage_test.cc
+++ src/test/data_storage_test.cc
@@ -114,3 +114,34 @@ TEST(DataStorageTest, PlainArrayTest1) {
     EXPECT_EQ(data[ii], ii + 1.0);
   }
 }
+
+TEST(DataStorageTest, PlainArrayTest2) {
+  typedef DenseVector<double> Vec;
+
+  double data[] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
+  int num_points = 3;
+  int dim = 2;
+
+  PlainArrayDataStorage<Vec> ds(data, num_points, dim);
+
+  ASSERT_EQ(ds.size(), num_points);
+  
+  std::vector<int32_t> keys = {0, 2};
+  auto iter = ds.get_subsequence(keys);
+
+  ASSERT_TRUE(iter.is_valid());
+  EXPECT_EQ(iter.get_point()[0], 1.0);
+  EXPECT_EQ(iter.get_point()[1], 2.0);
+
+  ++iter;
+  ASSERT_TRUE(iter.is_valid());
+  EXPECT_EQ(iter.get_point()[0], 5.0);
+  EXPECT_EQ(iter.get_point()[1], 6.0);
+  
+  ++iter;
+  ASSERT_FALSE(iter.is_valid());
+
+  for (int ii = 0; ii < num_points; ++ii) {
+    EXPECT_EQ(data[ii], ii + 1.0);
+  }
+}
