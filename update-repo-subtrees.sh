189fe80b4e521f6b61d1a8cab10d35791ca6f5ed
Ilya Razenshteyn <ilyaraz@mit.edu>
2017-07-20 05:11:33 -0500

    added pybind11 as an external dependency

--- update-repo-subtrees.sh
+++ update-repo-subtrees.sh
@@ -2,3 +2,4 @@
 git subtree pull --prefix src/include/falconn/ffht https://github.com/FALCONN-LIB/FFHT.git master --squash
 git subtree pull --prefix external/eigen https://github.com/FALCONN-LIB/eigen-mirror.git master --squash
 git subtree pull --prefix external/googletest https://github.com/google/googletest.git master --squash
+git subtree pull --prefix external/pybind11 https://github.com/pybind/pybind11.git master --squash
