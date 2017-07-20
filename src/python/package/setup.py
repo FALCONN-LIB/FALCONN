ca88c2cc5494de1dcacd9251e4bf08569b0c8b3b
Ilya Razenshteyn <ilyaraz@mit.edu>
2017-07-20 05:07:54 -0500

    changed the working version from 1.2.2 to 2.0.0, since we are updating quite a few things and breaking the backward compatibility

--- src/python/package/setup.py
+++ src/python/package/setup.py
@@ -35,7 +35,7 @@ module = Extension(
 
 setup(
     name='FALCONN',
-    version='1.2.2',
+    version='2.0.0',
     author='Ilya Razenshteyn, Ludwig Schmidt',
     author_email='falconn.lib@gmail.com',
     url='http://falconn-lib.org/',
