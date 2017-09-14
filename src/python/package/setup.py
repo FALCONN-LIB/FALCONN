4d737cbdf18a77b2a297f2ced4b37f8f9202f914
Ilya Razenshteyn <ilyaraz@mit.edu>
2017-09-14 14:54:17 -0400

    new version is 1.3.0, not 2.0.0

--- src/python/package/setup.py
+++ src/python/package/setup.py
@@ -28,7 +28,7 @@ module = Extension(
 
 setup(
     name='FALCONN',
-    version='2.0.0',
+    version='1.3.0',
     author='Ilya Razenshteyn, Ludwig Schmidt',
     author_email='falconn.lib@gmail.com',
     url='http://falconn-lib.org/',
